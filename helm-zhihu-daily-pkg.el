7354a56c9a6cf8de8fe50562bcbc4f731cef08bd
Chunyang Xu <xuchunyang56@gmail.com>
2015-04-22 22:14:35 +0800

    Delete generated *-pkg.el

--- helm-zhihu-daily-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-zhihu-daily" "0.1" "Helm interface for 知乎日报 (http://daily.zhihu.com)" '((helm "1.0") (cl-lib "0.5") (emacs "24.4")) :url "https://github.com/xuchunyang/helm-zhihu-daily")
